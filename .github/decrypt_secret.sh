def188f8186f406180bf01e9da61d0bbe882203c
Igor Loncarevic <acc@loncarevic.net>
2025-04-04 01:32:52 +0200

    Fix decrypt script

--- .github/decrypt_secret.sh
+++ .github/decrypt_secret.sh
@@ -5,8 +5,8 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRETS_PASSPHRASE" \
---output $HOME/work/web-tests/web-tests/secrets/secret.json secrets/secret.json.gpg
+--output $HOME/work/cypress-example/cypress-example/secrets/secret.json secrets/secret.json.gpg
 
 echo Decrypted file should appear in the following directory as secrets.json
-echo ls $HOME/work/web-tests/web-tests/secrets/
-ls $HOME/work/web-tests/web-tests/secrets/
+echo ls $HOME/work/cypress-example/cypress-example/secrets/
+ls $HOME/work/cypress-example/cypress-example/secrets/
